8f7063d8cab4083a30a832d73dcc7c33df4f5f6b
Silent Yang <silentyangyang@gmail.com>
2017-10-21 22:12:13 +0800

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,4 @@ if [ -z "$SSPASSWORD" ]; then
     echo >&2 '  i need a SSPASSWORD'
     exit 1
 fi
-echo '{ "server":"0.0.0.0", "server_port":10000, "local_port":1080, "password":"'$SSPASSWORD'", "timeout":600, "method":"'$SSMETHOD'" }' > config.json
-ssserver
+ss-server -s 0.0.0.0 -p 10000 -k $SSPASSWORD -m $SSMETHOD -u
